cc2352229ed4debda848466bec51aad9b44100a6
Mohamed Abdeen <mohamedabden21@gmail.com>
2023-12-06 11:26:08 +0200

    day06 refactor

--- day06/src/part2.rs
+++ day06/src/part2.rs
@@ -1,54 +1,32 @@
+use itertools::{self, Itertools};
 use std::fs;
 
 fn parse(input: String) -> (u64, u64) {
-    let split = input.split_once("\n").unwrap();
-    (
-        split
-            .0
-            .split_whitespace()
-            .skip(1)
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>()
-            .concat()
-            .parse()
-            .unwrap(),
-        split
-            .1
-            .split_whitespace()
-            .skip(1)
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>()
-            .concat()
-            .parse()
-            .unwrap(),
-    )
-}
-
-fn possible_distances(max_time: u64, to_beat: u64) -> u32 {
-    let mut ans = 0;
-    // peak of quadratic function is at max_time/2
-    // start from there and walk both ways
-    for x in (0..max_time / 2).rev() {
-        let distance = x * (max_time - x);
-        if distance <= to_beat {
-            break;
-        }
-        ans += 1;
-    }
-    for x in max_time / 2..=max_time {
-        let distance = x * (max_time - x);
-        if distance <= to_beat {
-            break;
-        }
-        ans += 1
-    }
-    return ans;
+    input
+        .split_terminator("\n")
+        .map(|line| {
+            line.split_whitespace()
+                .skip(1)
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+                .concat()
+                .parse()
+                .unwrap()
+        })
+        .collect_tuple()
+        .unwrap()
 }
 
 pub fn run(filename: &str) -> u32 {
     let input = fs::read_to_string(filename).unwrap();
-    let race = parse(input);
-    possible_distances(race.0, race.1)
+    let (time, distance) = parse(input);
+
+    (0..time)
+        .filter_map(|x| {
+            let my_distance = x * (time - x);
+            (my_distance > distance).then_some(my_distance)
+        })
+        .count() as u32
 }
 
 #[cfg(test)]
