f6c9c185286bccd4fa7824fbf66bc1e9e6dcccb6
Mohamed Abdeen <mohamedabden21@gmail.com>
2023-12-07 17:53:09 +0200

    day07 refactor

--- day07/src/part1.rs
+++ day07/src/part1.rs
@@ -1,4 +1,6 @@
-use std::{cmp::Ordering, collections::HashMap, fs};
+use std::{cmp::Ordering, fs, ops::Deref};
+
+use itertools::Itertools;
 
 #[derive(PartialOrd, PartialEq, Eq)]
 struct Hand {
@@ -7,28 +9,37 @@ struct Hand {
 }
 
 impl Hand {
-    fn new(card_string: &str, scoring: &HashMap<char, u8>) -> Hand {
-        let mut hand = [0; 13];
+    fn new(card_string: &str) -> Hand {
         let cards: [u8; 5] = card_string
             .chars()
-            .map(|card| scoring[&card])
+            .map(|card| match card {
+                'A' => 14,
+                'K' => 13,
+                'Q' => 12,
+                'J' => 11,
+                'T' => 10,
+                _ => card.to_digit(10).unwrap().try_into().unwrap(),
+            })
             .collect::<Vec<u8>>()
             .try_into()
             .unwrap();
 
-        cards.iter().for_each(|&card| hand[card as usize] += 1);
+        let hand: String = cards
+            .iter()
+            .counts()
+            .values()
+            .sorted()
+            .map(|ch| ch.to_string())
+            .collect();
 
-        hand.sort_by(|a, b| b.cmp(a));
-        let freq: [u8; 5] = hand[0..5].try_into().unwrap();
-
-        let score = match freq {
-            [1, 1, 1, 1, 1] => 0, // High Card
-            [2, 1, 1, 1, 0] => 1, // One Pair
-            [2, 2, 1, 0, 0] => 2, // Two Pair
-            [3, 1, 1, 0, 0] => 3, // Three of a kind
-            [3, 2, 0, 0, 0] => 4, // Full House
-            [4, 1, 0, 0, 0] => 5, // Four of a kind
-            [5, 0, 0, 0, 0] => 6, // Five of a kind
+        let score = match hand.deref() {
+            "11111" => 0, // High Card
+            "1112" => 1,  // One Pair
+            "122" => 2,   // Two Pair
+            "113" => 3,   // Three of a kind
+            "23" => 4,    // Full House
+            "14" => 5,    // Four of a kind
+            "5" => 6,     // Five of a kind
             _ => panic!("Invalid hand"),
         };
 
@@ -53,29 +64,13 @@ impl Ord for Hand {
 
 pub fn run(filename: &str) -> u32 {
     let input = fs::read_to_string(filename).unwrap();
-    let scoring: HashMap<char, u8> = HashMap::from([
-        ('2', 0),
-        ('3', 1),
-        ('4', 2),
-        ('5', 3),
-        ('6', 4),
-        ('7', 5),
-        ('8', 6),
-        ('9', 7),
-        ('T', 8),
-        ('J', 9),
-        ('Q', 10),
-        ('K', 11),
-        ('A', 12),
-    ]);
-    let mut rounds: Vec<(Hand, u32)> = input
+    input
         .split_terminator("\n")
         .map(|round| round.split_once(" ").unwrap())
-        .map(|(cards, bid)| (Hand::new(cards, &scoring), bid.parse().unwrap()))
-        .collect();
-    rounds.sort_by(|(hand1, _), (hand2, _)| hand1.cmp(&hand2));
-    rounds
+        .map(|(cards, bid)| (Hand::new(cards), bid.parse().unwrap()))
+        .collect::<Vec<(Hand, u32)>>()
         .iter()
+        .sorted_by(|(hand1, _), (hand2, _)| hand1.cmp(&hand2))
         .enumerate()
         .map(|(i, (_, score))| score * (i as u32 + 1))
         .sum()
--- day07/src/part2.rs
+++ day07/src/part2.rs
@@ -1,4 +1,5 @@
-use std::{cmp::Ordering, collections::HashMap, fs};
+use itertools::Itertools;
+use std::{cmp::Ordering, fs, ops::Deref};
 
 #[derive(PartialOrd, PartialEq, Eq)]
 struct Hand {
@@ -7,37 +8,42 @@ struct Hand {
 }
 
 impl Hand {
-    fn new(card_string: &str, scoring: &HashMap<char, u8>) -> Hand {
-        let mut hand = [0; 13];
+    fn new(card_string: &str) -> Hand {
+        let mut freq = Vec::from([0; 15]);
         let cards: [u8; 5] = card_string
             .chars()
-            .map(|card| scoring[&card])
+            .map(|card| match card {
+                'A' => 14,
+                'K' => 13,
+                'Q' => 12,
+                'J' => 1,
+                'T' => 10,
+                _ => card.to_digit(10).unwrap().try_into().unwrap(),
+            })
             .collect::<Vec<u8>>()
             .try_into()
             .unwrap();
 
-        let mut jokers = 0;
-        cards.iter().for_each(|&card| {
-            if card != scoring[&'J'] {
-                hand[card as usize] += 1
-            } else {
-                jokers += 1
-            }
-        });
+        cards.iter().for_each(|&card| freq[card as usize] += 1);
+        let jokers = freq.remove(1);
 
-        hand.sort_by(|a, b| b.cmp(a));
-        hand[0] += jokers;
-        let freq: [u8; 5] = hand[0..5].try_into().unwrap();
+        freq.sort_by(|a, b| b.cmp(a));
+        freq[0] += jokers;
+        let hand: String = freq[0..5]
+            .iter()
+            .filter(|&&num| num != 0)
+            .map(|num| num.to_string())
+            .collect::<String>();
 
-        let score = match freq {
-            [1, 1, 1, 1, 1] => 0, // High Card
-            [2, 1, 1, 1, 0] => 1, // One Pair
-            [2, 2, 1, 0, 0] => 2, // Two Pair
-            [3, 1, 1, 0, 0] => 3, // Three of a kind
-            [3, 2, 0, 0, 0] => 4, // Full House
-            [4, 1, 0, 0, 0] => 5, // Four of a kind
-            [5, 0, 0, 0, 0] => 6, // Five of a kind
-            _ => panic!("Invalid hand"),
+        let score = match hand.deref() {
+            "11111" => 0, // High Card
+            "2111" => 1,  // One Pair
+            "221" => 2,   // Two Pair
+            "311" => 3,   // Three of a kind
+            "32" => 4,    // Full House
+            "41" => 5,    // Four of a kind
+            "5" => 6,     // Five of a kind
+            _ => panic!("Invalid hand: {}, {}", hand, jokers),
         };
 
         Hand { cards, score }
@@ -61,29 +67,13 @@ impl Ord for Hand {
 
 pub fn run(filename: &str) -> u32 {
     let input = fs::read_to_string(filename).unwrap();
-    let scoring: HashMap<char, u8> = HashMap::from([
-        ('J', 0),
-        ('2', 1),
-        ('3', 2),
-        ('4', 3),
-        ('5', 4),
-        ('6', 5),
-        ('7', 6),
-        ('8', 7),
-        ('9', 8),
-        ('T', 9),
-        ('Q', 10),
-        ('K', 11),
-        ('A', 12),
-    ]);
-    let mut rounds: Vec<(Hand, u32)> = input
+    input
         .split_terminator("\n")
         .map(|round| round.split_once(" ").unwrap())
-        .map(|(cards, bid)| (Hand::new(cards, &scoring), bid.parse().unwrap()))
-        .collect();
-    rounds.sort_by(|(hand1, _), (hand2, _)| hand1.cmp(&hand2));
-    rounds
+        .map(|(cards, bid)| (Hand::new(cards), bid.parse().unwrap()))
+        .collect::<Vec<(Hand, u32)>>()
         .iter()
+        .sorted_by(|(hand1, _), (hand2, _)| hand1.cmp(&hand2))
         .enumerate()
         .map(|(i, (_, score))| score * (i as u32 + 1))
         .sum()
@@ -98,4 +88,3 @@ mod tests {
         assert_eq!(run(input), 5905);
     }
 }
-
