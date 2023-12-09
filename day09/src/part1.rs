99cabf979a94b343da1cc73cf6378e87d40e4afa
Mohamed Abdeen <mohamedabden21@gmail.com>
2023-12-09 10:05:10 +0200

    day09 refactor

--- day09/src/part1.rs
+++ day09/src/part1.rs
@@ -1,17 +1,13 @@
 use itertools::Itertools;
 use std::fs;
 
-fn extrapolate(pattern: &Vec<i64>, to_add: i64) -> i64 {
-    return pattern.last().unwrap() + to_add;
-}
-
-fn find_pattern(pattern: &Vec<i64>) -> i64 {
+fn extrapolate(pattern: Vec<i64>) -> i64 {
     if pattern.iter().all(|&x| x == 0) {
-        return extrapolate(&pattern, 0);
+        return 0;
     }
 
     let inner_pattern = pattern.iter().tuple_windows().map(|(a, b)| b - a).collect();
-    return extrapolate(&pattern, find_pattern(&inner_pattern));
+    return pattern.last().unwrap() + extrapolate(inner_pattern);
 }
 
 pub fn run(filename: &str) -> i64 {
@@ -23,7 +19,7 @@ pub fn run(filename: &str) -> i64 {
                 .map(|x| x.parse().unwrap())
                 .collect::<Vec<i64>>()
         })
-        .map(|line| find_pattern(&line))
+        .map(|pattern| extrapolate(pattern))
         .sum()
 }
 
--- day09/src/part2.rs
+++ day09/src/part2.rs
@@ -1,17 +1,13 @@
 use itertools::Itertools;
 use std::fs;
 
-fn extrapolate(pattern: &Vec<i64>, to_sub: i64) -> i64 {
-    return pattern.first().unwrap() - to_sub;
-}
-
-fn find_pattern(pattern: &Vec<i64>) -> i64 {
+fn extrapolate(pattern: Vec<i64>) -> i64 {
     if pattern.iter().all(|&x| x == 0) {
-        return extrapolate(&pattern, 0);
+        return 0;
     }
 
     let inner_pattern = pattern.iter().tuple_windows().map(|(a, b)| b - a).collect();
-    return extrapolate(&pattern, find_pattern(&inner_pattern));
+    return pattern.first().unwrap() - extrapolate(inner_pattern);
 }
 
 pub fn run(filename: &str) -> i64 {
@@ -23,7 +19,7 @@ pub fn run(filename: &str) -> i64 {
                 .map(|x| x.parse().unwrap())
                 .collect::<Vec<i64>>()
         })
-        .map(|line| find_pattern(&line))
+        .map(|pattern| extrapolate(pattern))
         .sum()
 }
 
